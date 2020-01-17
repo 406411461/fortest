624119aac075815c90af6976784886061ae718c7
Kobe <brianwu82813041@gmail.com>
2020-01-17 10:54:27 +0800

    add pipe.c

---
+++ d/pipe.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+int main(){
+    pid_t pid;
+    pid  = fork();
+    printf("Hello World!!!\n");
+    if(pid>0){
+
+
+    }
+
+    else if(pid==0){
+
+
+    }
+
+
+}
